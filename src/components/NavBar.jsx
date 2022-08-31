50c790a54b787e40808133f89b50e8a8bf05609e
Chibu <atuchibueze@gmail.com>
2022-08-31 12:03:29 -0400

    set up projects page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 import { SocialIcon } from "react-social-icons";
 
 const NavBar = () => {
-  const navLinkSyle1 = ({ isActive }) => {
+  const navLinkStyle1 = ({ isActive }) => {
     return {
       color: isActive ? "white" : "#fff5f5",
     };
   };
 
-  const navLinkSyle2 = ({ isActive }) => {
+  const navLinkStyle2 = ({ isActive }) => {
     return {
       color: isActive ? "#fff5f5" : "#fed7d7",
       backgroundColor: isActive ? "#c53030" : "",
@@ -23,28 +23,28 @@ const NavBar = () => {
           <NavLink
             to="/"
             exact="true"
-            style={navLinkSyle2}
+            style={navLinkStyle1}
             className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
           >
             Chibu
           </NavLink>
           <NavLink
             to="/post"
-            style={navLinkSyle2}
+            style={navLinkStyle2}
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
           >
             Blog Posts
           </NavLink>
           <NavLink
             to="/project"
-            style={navLinkSyle2}
+            style={navLinkStyle2}
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
           >
             Projects
           </NavLink>
           <NavLink
             to="/about"
-            style={navLinkSyle2}
+            style={navLinkStyle2}
             className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
           >
             About Me!
